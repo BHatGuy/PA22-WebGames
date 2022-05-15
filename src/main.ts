34bf47c371f3696a80cd47d8fc1e1008c9e960c7
Max Viehl <ib_1@web.de>
2022-05-15 16:08:57 +0200

    Add dt to update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 var canvas: HTMLCanvasElement;
 var context: CanvasRenderingContext2D;
 
+// game variables
 var x = 100;
+var v = 200; // pixels per second
 
+// system variables
+var last_tick_t = 0;
 var width: number;
 var height: number;
 
@@ -12,15 +16,19 @@ function draw() {
     context.strokeRect(x, 100, 100, 100);
 }
 
-function update() {
-    x += 1;
+function update(dt: number) {
+    x += v * dt;
     if (x > width - 200) {
         x = 100;
     }
 }
 
-function loop() {
-    update();
+function loop(t_ms: number) {
+    // calculate dt
+    let dt = t_ms - last_tick_t;
+    last_tick_t = t_ms;
+
+    update(dt / 1000); // to seconds
     draw();
 
     window.requestAnimationFrame(loop);
@@ -39,7 +47,7 @@ function main() {
     window.addEventListener("resize", resized);
     resized();
 
-    loop();
+    loop(performance.now());
 }
 
 
